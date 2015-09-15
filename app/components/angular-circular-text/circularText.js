9b05734d642adee29e44e9d6395cd17d34c1e74f
Aidan Gee <aidangeewd@gmail.com>
2015-09-15 10:14:57 +0100

    Updated working versions using jquery and the arctext.js jquery plugin, added these dependencies to bower. Know issues include the use of ng-pattern and ng-maxlength. Also need to remove the nasty look up for a h3

--- app/components/angular-circular-text/circularText.js
+++ app/components/angular-circular-text/circularText.js
@@ -1,9 +1,18 @@
-angular.module('circularText', ['ngLettering'])
-.controller('testCtrl', ['$scope', function ($scope) {
-	$scope.testInput;
-}])
+//
+//  arcText module implementing for implementing an angular version of the arctext.js jquery plguing
+//  Note: this module will require arcText.jquery.js (https://github.com/codrops/Arctext) and jquery to run
+//
+
+angular.module('circularText', [])
+//
+// Fiter lettering
+//
+// Takes a string input and outputs each character as a html span element
+// Essentially emulating some functionality from Lettering.js (https://github.com/davatron5000/Lettering.js)
+//
 .filter('lettering', function($sce) {
   return function(input) {
+  	console.log('input', input);
   	if (typeof input !== 'undefined'){
   		var lettering = '';
   		var letters = input.split('');
@@ -17,30 +26,43 @@ angular.module('circularText', ['ngLettering'])
   	else return "";
   };
 })
-.filter('replaceWhitespace', function() {
-  return function(input) {
-  	if (typeof input !== 'undefined'){
-  		return input.replace(/^\s+|\s+$/g, '').replace(/\s/g, '&nbsp;');
-  	}
-  	else return "";
-  };
-})
-.directive('circularText', function($filter) {
-	var replaceWhitespace = $filter('replaceWhitespace')
+
+//
+// directive arcText
+//
+// Makes use of the arcText jquery plugin to create a data-bindinded version that will work with text inputs
+// see https://github.com/codrops/Arctext
+//
+.directive('circularText',['$filter', '$timeout', function($filter, $timeout) {
 	return {
 		restrict: 'E',
-		templateUrl: '/app/components/angular-circular-text/circularText.html',
+		template: '<h3 ng-bind-html="text | lettering"></h3>',
 		scope: {
 			text: '=ngModel',
-			radius: '='
+			radius: '=',
+      enabled: '='
 		},
 		link: function(scope, element, attrs){
-			var delta = (180 / Math.PI),
-		        fs = parseInt(element.css('font-size'), 10),
-		        ch = parseInt(element.css('line-height'), 10) || fs,
-		      	txt = replaceWhitespace(scope.text),
-		        letters, 
-		        center;
+
+      var initCurve = function(){
+        angular.element(document).ready(function() {
+          var el = $(element).find( "h3" );
+          el.arctext({radius:200});
+          scope.$watch('text', function(newValue, oldValue, scope) {
+              el.arctext('destroy');
+              el.arctext({radius:200});
+          });
+        });
+      }
+
+      // if enabled is used check for 
+      if(angular.isDefined(attrs.enabled) && scope.enabled){
+        initCurve();
+      } 
+      else if(angular.isUndefined(attrs.enabled)){
+        initCurve();
+      }
+			
 		}
 	}
-});
+}]);
---
+++ app/test/testController.js
@@ -0,0 +1,5 @@
+angular.module('testApp', ['circularText'])
+
+.controller('testCtrl', function(){
+	
+});
